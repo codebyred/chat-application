8cac67e7dba9db25ba905cde5cb2692cfbb9a58b
Redoan <nazmulhaqueredoan@gmail.com>
2022-03-29 21:52:03 +0600

    updating login-view

--- src/main/java/com/example/message_server/LoginController.java
+++ src/main/java/com/example/message_server/LoginController.java
@@ -6,18 +6,29 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
     @FXML
     private Button login_btn;
+    @FXML
+    private TextField user_name;
     @Override
     public void initialize(URL location, ResourceBundle resources){
+        ArrayList<String> users = new ArrayList<>();
+        users.add("Sheikh");
         login_btn.setOnAction((event)->{
+            String userName = user_name.getText();
+            if(!users.contains(userName)){
+                return;
+            }
             try {
                 FXMLLoader fxmlLoader2 = new FXMLLoader(App.class.getResource("hello-view.fxml"));
                 Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
